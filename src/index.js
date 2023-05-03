cf16f4930c4c824930236c6d93e3557e6451dcb5
Andrea Cox <msandrea712@yahoo.com>
2023-05-03 14:25:46 -0400

    ramen rater

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// write your code here
+let ramenMenuDiv=document.querySelector("#ramen-menu")
+function menu(){
+    fetch(`http://localhost:3000/ramens`)
+    .then(response=>{
+return response.json()
+    }).then(data=>{
+        console.log(data)
+        data.map(item=>{
+
+let imageElement=document.createElement("img")
+imageElement.src=item.image
+ramenMenuDiv.appendChild(imageElement)
+        })
+    })
+}
+menu()
